373b9f9486c3aa08a3af750a1881b576018b2378
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2024-08-13 22:10:10 +0200

    ajustes css

--- src/components/ui/Welcome.jsx
+++ src/components/ui/Welcome.jsx
@@ -7,15 +7,15 @@ function Welcome() {
     return (
         <>
             <main>
-                <div className="head">
-                    <img className="logo" src={logoTxt} />
+                <div className="head home">
+                    <img className="logo-home" src={logoTxt} />
                     <img className="icon" src={icon} />
                 </div>
                 <div className="body">
                     <h2>Wellcome</h2>
                     <h3>Enter Let's Play and check the results of your favorite competitions!!!</h3>
                     <button>
-                        <Link>See results</Link>
+                        <Link to="/results">See results</Link>
                     </button>
                 </div>
 
